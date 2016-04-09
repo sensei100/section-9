6415506d63235b94c82be4105c9b3094111023ea
Sensei100 <jenanton01@gmail.com>
2016-04-09 17:43:01 +0300

    first commit

---
+++ exercise.js
@@ -0,0 +1,52 @@
+/* var count = -10;
+while(count < 20) {
+	console.log(count);
+	count ++;
+} 
+
+var counter = 10;
+
+while (counter <= 40) {
+	if (counter % 2 === 0) {
+		console.log(counter);
+	}
+		counter +=1; 	
+} 
+
+var three = 300;
+while (three <= 333) {
+	if (three % 2 !== 0) {
+		console.log(three);	
+	}
+	three +=1;
+} 
+
+var count = 5;
+while (count <= 50) {
+	if (count % 5 === 0 && count % 3 === 0) {
+		console.log(count);	
+	}
+	count+=1;
+} */
+
+for (var i = -10; i < 20; i++) {
+	console.log(i);
+}
+
+for (var i = 10; i <= 40; i++) {
+	if (i % 2 === 0) {
+		console.log(i);
+	}
+}
+
+for (var i = 300; i <= 333; i++) {
+	if (i % 2 !== 0) {
+		console.log(i);
+	}
+}
+
+for (var i = 5; i <= 50; i++) {
+	if (i % 5 === 0 && i % 3 === 0) {
+		console.log(i);
+	}
+}
